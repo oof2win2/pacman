82aacc51e301e96e593556f68589d50aeafad434
oof2win2 <honza.kocourek.parklane@gmail.com>
2020-09-17 19:46:36 +0200

    Initial Commit

---
+++ pacman/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pacman
+//
+//  Created by Honza on 17/09/2020.
+//  Copyright © 2020 oof2win2. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
